0b76cfe41d7bb3aff9b7ccded48af5d65928382b
qianli lu <georgeluuol@163.com>
2022-02-18 14:10:22 +0800

    重构

---
+++ quartz-job/src/main/java/com/lu/quartzjob/job/SampleJob.java
@@ -0,0 +1,18 @@
+package com.lu.quartzjob.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class SampleJob extends QuartzJobBean {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println("Quartz ---->  Hello, " + this.name);
+    }
+}
